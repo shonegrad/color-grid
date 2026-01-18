1286be3578f9c94c58759e760eaa1d52589c497d
Nebojsa Despotovic <nebojsadespotovic@Nebojsas-Laptop.local>
2026-01-18 15:15:20 -0500

    test: log boot message from PaletteApp to verify runtime

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import { PaletteApp } from './PaletteApp'
 import './styles.css'
 
+console.log('[Palette] booting', new Date().toISOString())
+
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <PaletteApp />
